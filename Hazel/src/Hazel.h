76e6d3a8b15a9a5ab40b0d652dcb76bd6746a361
MrFirelord <firelord17@gmail.com>
2021-07-12 01:36:58 -0400

    Setup basic Application and Entry Point.

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// For use by Hazel Application
+
+#include "Hazel/Application.h"
+#include "Hazel/EntryPoint.h"
---
+++ Sandbox/SandboxApp.cpp
@@ -0,0 +1,16 @@
+#include <Hazel.h>
+
+class Sanbox : public Hazel::Application {
+public:
+	Sanbox() {
+
+	}
+
+	~Sanbox() {
+
+	}
+};
+
+Hazel::Application* Hazel::CreateApplication() {
+	return new Sanbox();
+}
