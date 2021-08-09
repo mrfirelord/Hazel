91f16ec92beb46bfc7a1c6c982724fb24009996f
MrFirelord <firelord17@gmail.com>
2021-08-09 02:51:16 -0400

    Introduct BufferLayout abstraction

--- Hazel/src/Platform/OpenGL/OpenGLBuffer.h
+++ Hazel/src/Platform/OpenGL/OpenGLBuffer.h
@@ -11,8 +11,12 @@ namespace Hazel {
 
 		virtual void Bind() const;
 		virtual void Unbind() const;
+
+		virtual const BufferLayout GetLayout() const { return m_Layout; }
+		virtual void SetLayout(const BufferLayout& layout) { m_Layout = layout; }
 	private:
 		uint32_t m_RendererID;
+		BufferLayout m_Layout;
 	};
 
 	class OpenGLIndexBuffer : public IndexBuffer {
@@ -20,8 +24,8 @@ namespace Hazel {
 		OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
 		virtual ~OpenGLIndexBuffer();
 
-		virtual void Bind() const;
-		virtual void Unbind() const;
+		virtual void Bind() const override;
+		virtual void Unbind() const override;
 
 		virtual uint32_t GetCount() const { return m_Count; }
 	private:
