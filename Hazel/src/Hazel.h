b356dd1b9ad8b84755cfebe2e455dc307a8fa21b
MrFirelord <firelord17@gmail.com>
2021-08-10 20:03:21 -0400

    Move example code to Sandbox

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -11,4 +11,11 @@
 #include "Hazel/KeyCodes.h"
 #include "Hazel/MouseButtonCodes.h"
 
+#include "Hazel/Renderer/Buffer.h"
+#include "Hazel/Renderer/OrthographicCamera.h"
+#include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/RenderCommand.h"
+#include "Hazel/Renderer/Shader.h"
+#include "Hazel/Renderer/VertexArray.h"
+
 #include "Hazel/EntryPoint.h"
--- Hazel/src/Hazel/Application.h
+++ Hazel/src/Hazel/Application.h
@@ -31,20 +31,12 @@ namespace Hazel {
 		inline Window& GetWindow() { return *m_Window; }
 
 		inline static Application& Get() { return *s_Instance; }
-
-		std::shared_ptr<Shader> m_Shader;
-		std::shared_ptr<VertexArray> m_VertexArray;
-
-		std::shared_ptr<Shader> m_BlueShader;
-		std::shared_ptr<VertexArray> m_SquareVA;
 	private:
 		std::unique_ptr<Window> m_Window;
 		ImGuiLayer* m_ImGuiLayer;
 		bool m_Running = true;
 		LayerStack m_LayerStack;
 		static Application* s_Instance;
-
-		OrthographicCamera m_Camera;
 	};
 
 	// To be defined in CLIENT
