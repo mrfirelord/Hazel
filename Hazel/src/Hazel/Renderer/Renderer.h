4fcdb47889ccb6b8097f4dc52bf30c4f0858e93e
MrFirelord <firelord17@gmail.com>
2021-08-11 08:20:49 -0400

    Add transform support to Renderer

--- Hazel/src/Hazel/Renderer/Renderer.h
+++ Hazel/src/Hazel/Renderer/Renderer.h
@@ -11,7 +11,10 @@ namespace Hazel {
 
 		static void EndScene();
 
-		static void Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray);
+		static void Submit(
+			const std::shared_ptr<Shader>& shader,
+			const std::shared_ptr<VertexArray>& vertexArray,
+			const glm::mat4 transform = glm::mat4(1.0));
 
 		inline static RendererAPI::API GetAPI() { return RendererAPI::GetAPI(); }
 	private:
--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -3,6 +3,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class ExampleLayer : public Hazel::Layer {
 public:
 	ExampleLayer() : Layer("Example"), m_Camera(-1.6f, 1.6f, -0.9f, 0.9f), m_CameraPosition(0), m_Rotation(0) {
@@ -31,10 +33,10 @@ public:
 		m_SquareVA.reset(Hazel::VertexArray::Create());
 
 		float squareVertices[3 * 4] = {
-			-0.75f, -0.75f, 0.0f,
-			 0.75f, -0.75f, 0.0f,
-			 0.75f,  0.75f, 0.0f,
-			-0.75f,  0.75f, 0.0f
+			-0.5f, -0.5f, 0.0f,
+			 0.5f, -0.5f, 0.0f,
+			 0.5f,  0.5f, 0.0f,
+			-0.5f,  0.5f, 0.0f
 		};
 
 		std::shared_ptr<Hazel::VertexBuffer> squareVB;
@@ -56,6 +58,7 @@ public:
 			layout(location=1) in vec4 a_Color;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 			out vec4 v_Color;
@@ -63,7 +66,7 @@ public:
 			void main() {
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
 			}
 		)";
 
@@ -89,13 +92,14 @@ public:
 			layout(location = 0) in vec3 a_Position;
 
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;
 
 			void main()
 			{
 				v_Position = a_Position;
-				gl_Position = u_ViewProjection * vec4(a_Position, 1.0);	
+				gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);	
 			}
 		)";
 
@@ -118,7 +122,7 @@ public:
 			m_CameraPosition.x += m_CameraSpeed * ts;
 		else if (Hazel::Input::isKeyPressed(HZ_KEY_LEFT))
 			m_CameraPosition.x -= m_CameraSpeed * ts;
-		
+
 		if (Hazel::Input::isKeyPressed(HZ_KEY_UP))
 			m_CameraPosition.y -= m_CameraSpeed * ts;
 		else if (Hazel::Input::isKeyPressed(HZ_KEY_DOWN))
@@ -137,7 +141,16 @@ public:
 
 		Hazel::Renderer::BeginScene(m_Camera);
 
-		Hazel::Renderer::Submit(m_BlueShader, m_SquareVA);
+		glm::mat4 scale = glm::scale(glm::mat4(1.0), glm::vec3(0.1));
+
+		for (int y = 0; y < 20; y++) {
+			for (int x = 0; x < 20; x++) {
+				glm::vec3 pos(x * 0.11, y * 0.11, 0);
+				glm::mat4 transform = glm::translate(glm::mat4(1.0), pos) * scale;
+				Hazel::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+			}
+		}
+
 		Hazel::Renderer::Submit(m_Shader, m_VertexArray);
 
 		Hazel::Renderer::EndScene();
