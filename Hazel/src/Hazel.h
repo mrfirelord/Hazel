0dfa1d24ca67120c8ff5305b09f0914bcf04a6ea
MrFirelord <firelord17@gmail.com>
2021-08-18 22:49:48 -0400

    Create 2D renderer

--- Hazel/src/Hazel.h
+++ Hazel/src/Hazel.h
@@ -15,6 +15,7 @@
 #include "Hazel/Renderer/OrthographicCamera.h"
 #include "Hazel/Renderer/OrthographicCameraController.h"
 #include "Hazel/Renderer/Renderer.h"
+#include "Hazel/Renderer/Renderer2D.h"
 #include "Hazel/Renderer/RenderCommand.h"
 #include "Hazel/Renderer/Shader.h"
 #include "Hazel/Renderer/Texture.h"
---
+++ Hazel/src/Hazel/Renderer/Renderer2D.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Hazel/Renderer/OrthographicCamera.h"
+
+namespace Hazel {
+	class Renderer2D {
+	public:
+		static void Init();
+		static void Shutdown();
+
+		static void BeginScene(const OrthographicCamera& camera);
+		static void EndScene();
+
+		// Primities
+		static void DrawQuad(const glm::vec2 position, const glm::vec2 size, const glm::vec4 color);
+		static void DrawQuad(const glm::vec3 position, const glm::vec2 size, const glm::vec4 color);
+	};
+}
