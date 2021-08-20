85711d9fb1477ff1cd7fb669b4b6c06021b17b6e
MrFirelord <firelord17@gmail.com>
2021-08-20 01:06:44 -0400

    Combine shader for flat color and textured shader into one

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -29,10 +29,9 @@ void Sandbox2D::OnUpdate(Hazel::Timestep ts) {
 
 	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
+	Hazel::Renderer2D::DrawQuad({ -1.0, 0, 0.1 }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 	Hazel::Renderer2D::DrawQuad({ 0.0, -0.0 },    { 1.5f, 1.75f }, m_CheckerboardTexture);
-	Hazel::Renderer2D::DrawQuad({ -1.0, 0, 0.1 }, { 0.8f, 0.8f  }, { 0.8f, 0.2f, 0.3f, 1.0f });
-
-
+	
 	Hazel::Renderer2D::EndScene();
 
 	/*
