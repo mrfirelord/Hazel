0c2d5a571c38220fce93a4b4e0dae04dc5b8c5c8
MrFirelord <firelord17@gmail.com>
2021-08-21 21:23:00 -0400

    Initial instrumentation work

--- Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
+++ Hazel/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -84,10 +84,7 @@ namespace Hazel {
 		}
 	}
 
-	void ImGuiLayer::OnImGuiRender()
-	{
+	void ImGuiLayer::OnImGuiRender() {
 		static bool show = true;
-		ImGui::ShowDemoWindow(&show);
 	}
-
 }
--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -8,6 +8,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 
+
 Sandbox2D::Sandbox2D() : Layer("Sanbox2D"), m_CameraController(1.5f, true) {
 
 }
@@ -20,26 +21,29 @@ void Sandbox2D::OnDetach() {
 }
 
 void Sandbox2D::OnUpdate(Hazel::Timestep ts) {
-	// Update
-	m_CameraController.OnUpdate(ts);
+	HZ_PROFILE_FUNCTION();
+	{
+		HZ_PROFILE_SCOPE("CameraController.OnUpdate");
+		// Update
+		m_CameraController.OnUpdate(ts);
+	}
 
-	// Render
-	Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
-	Hazel::RenderCommand::Clear();
+	{
+		HZ_PROFILE_SCOPE("Renderer preparation");
+		// Render
+		Hazel::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
+		Hazel::RenderCommand::Clear();
+	}
 
-	Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
+	{
+		HZ_PROFILE_SCOPE("Rendering scene");
+		Hazel::Renderer2D::BeginScene(m_CameraController.GetCamera());
 
-	Hazel::Renderer2D::DrawQuad({ -1.0, 0, 0.1 }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-	Hazel::Renderer2D::DrawQuad({ 0.0, -0.0 },    { 1.5f, 1.75f }, m_CheckerboardTexture);
-	
-	Hazel::Renderer2D::EndScene();
+		Hazel::Renderer2D::DrawQuad({ -1.0, 0, 0.1 }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Hazel::Renderer2D::DrawQuad({ 0.0, -0.0 }, { 1.5f, 1.75f }, m_CheckerboardTexture);
 
-	/*
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->Bind();
-	std::dynamic_pointer_cast<Hazel::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat4("u_Color", m_SquareColor);
-
-	Hazel::Renderer::Submit(m_FlatColorShader, m_SquareVA, glm::mat4(1.0));
-	*/
+		Hazel::Renderer2D::EndScene();
+	}
 }
 
 void Sandbox2D::OnEvent(Hazel::Event& event) {
@@ -47,6 +51,8 @@ void Sandbox2D::OnEvent(Hazel::Event& event) {
 }
 
 void Sandbox2D::OnImGuiRender() {
+	HZ_PROFILE_FUNCTION();
+
 	ImGui::Begin("Settings");
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 	ImGui::End();
