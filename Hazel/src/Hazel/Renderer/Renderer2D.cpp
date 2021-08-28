1506aab4984fa550e459cafbb522abfcc2b211d1
MrFirelord <firelord17@gmail.com>
2021-08-27 22:54:58 -0400

    Renderer2D: added draw call splitting and stats

--- Hazel/src/Hazel/Renderer/Renderer2D.cpp
+++ Hazel/src/Hazel/Renderer/Renderer2D.cpp
@@ -17,9 +17,9 @@ namespace Hazel {
 	};
 
 	struct Renderer2DData {
-		const uint32_t MaxQuads = 10000;
-		const uint32_t MaxVertices = MaxQuads * 4;
-		const uint32_t MaxIndices = MaxQuads * 6;
+		static const uint32_t MaxQuads = 10000;
+		static const uint32_t MaxVertices = MaxQuads * 4;
+		static const uint32_t MaxIndices = MaxQuads * 6;
 		static const uint32_t MaxTextureSlots = 32;
 
 		Ref<VertexArray> QuadVertexArray;
@@ -36,6 +36,8 @@ namespace Hazel {
 		uint32_t TextureSlotIndex = 1; // 0 - White Texture
 
 		glm::vec4 QuadVertexPositions[4];
+
+		Renderer2D::Statistics Stats;
 	};
 
 	static Renderer2DData s_Data;
@@ -129,6 +131,7 @@ namespace Hazel {
 			s_Data.TextureSlots[i]->Bind(i);
 
 		RenderCommand::DrawIndexed(s_Data.QuadVertexArray, s_Data.QuadIndexCount);
+		s_Data.Stats.DrawCalls++;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2 position, const glm::vec2 size, const glm::vec4 color) {
@@ -138,6 +141,9 @@ namespace Hazel {
 	void Renderer2D::DrawQuad(const glm::vec3 position, const glm::vec2 size, const glm::vec4 color) {
 		HZ_PROFILE_FUNCTION();
 
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
+
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::scale(glm::mat4(1.0f), { size.x, size.y, 1.0f });
 
@@ -170,6 +176,7 @@ namespace Hazel {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+		s_Data.Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawQuad(const glm::vec2 position, const glm::vec2 size, Ref<Texture2D>& texture, float tilingFactor, const glm::vec4 tintColor) {
@@ -179,6 +186,9 @@ namespace Hazel {
 	void Renderer2D::DrawQuad(const glm::vec3 position, const glm::vec2 size, Ref<Texture2D>& texture, float tilingFactor, const glm::vec4 tintColor) {
 		HZ_PROFILE_FUNCTION();
 
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
+
 		constexpr glm::vec4 color = { 1.0f,1.0f, 1.0f, 1.0f };
 
 		float textureIndex = 0.0f;
@@ -227,6 +237,7 @@ namespace Hazel {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+		s_Data.Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2 position, const glm::vec2 size, float rotation, const glm::vec4 color) {
@@ -235,6 +246,9 @@ namespace Hazel {
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec3 position, const glm::vec2 size, float rotation, const glm::vec4 color) {
 		HZ_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
 
 		glm::mat4 transform = glm::translate(glm::mat4(1.0f), position)
 			* glm::rotate(glm::mat4(1.0f), glm::radians(rotation), { 0.0f, 0.0f, 1.0f })
@@ -269,6 +283,7 @@ namespace Hazel {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+		s_Data.Stats.QuadCount++;
 	}
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec2 position, const glm::vec2 size, float rotation, Ref<Texture2D>& texture, float tilingFactor, const glm::vec4 tintColor) {
@@ -277,6 +292,9 @@ namespace Hazel {
 
 	void Renderer2D::DrawRotatedQuad(const glm::vec3 position, const glm::vec2 size, float rotation, Ref<Texture2D>& texture, float tilingFactor, const glm::vec4 tintColor) {
 		HZ_PROFILE_FUNCTION();
+
+		if (s_Data.QuadIndexCount >= Renderer2DData::MaxIndices)
+			FlushAndReset();
 
 		constexpr glm::vec4 color = { 1.0f,1.0f, 1.0f, 1.0f };
 
@@ -327,5 +345,23 @@ namespace Hazel {
 		s_Data.QuadVertexBufferPtr++;
 
 		s_Data.QuadIndexCount += 6;
+		s_Data.Stats.QuadCount++;
+	}
+
+	void Renderer2D::ResetStats() {
+		memset(&s_Data.Stats, 0, sizeof(Statistics));
+	}
+
+	Renderer2D::Statistics Renderer2D::GetStats() {
+		return s_Data.Stats;
+	}
+
+	void Renderer2D::FlushAndReset() {
+		EndScene();
+
+		s_Data.QuadIndexCount = 0;
+		s_Data.QuadVertexBufferPtr = s_Data.QuadVertexBufferBase;
+
+		s_Data.TextureSlotIndex = 1;
 	}
 }
